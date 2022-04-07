4837cf482d37bfcd6a1b3207f5e9f910a1b17606
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-04-07 14:17:54 +0530

    Time: 50 ms (82.50%), Space: 14.4 MB (88.18%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,19 +1,31 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        listt=list(s)
-        ns,nt="",""
-        l=len(listt)
-        for i in range(l):
-            if listt[l-i-1].isalnum():
-                ns=ns+listt[l-i-1].lower()
-            # if listt[i].isalnum():
-            #     nt=nt+listt[i].lower()
-        # print(ns)
-        # print(nt)
-        if ns==ns[::-1]:
-            return True
-        else:
-            return False
+        # listt=list(s)
+        # ns,nt="",""
+        # l=len(listt)
+        # for i in range(l):
+        #     if listt[l-i-1].isalnum():
+        #         ns=ns+listt[l-i-1].lower()
+        #     # if listt[i].isalnum():
+        #     #     nt=nt+listt[i].lower()
+        # # print(ns)
+        # # print(nt)
+        # if ns==ns[::-1]:
+        #     return True
+        # else:
+        #     return False
+        alpha='qwertyuiopasdfghjklzxcvbnm1234567890'
+        new=''
+        # converting to lowercase
+        s=s.lower()
+        
+        # fetching the ascii characters
+        for i in range(len(s)):
+            if s[i] in alpha:
+                
+                new=new+s[i]
+        
+        return new==new[::-1]
                 
                 
             
