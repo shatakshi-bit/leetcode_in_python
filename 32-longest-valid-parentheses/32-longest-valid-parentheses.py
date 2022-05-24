b9cc9b9cc0a2af7dee9454ef6e25a1c05a70d209
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-05-24 17:54:27 +0530

    Time: 63 ms (48.59%), Space: 14 MB (99.13%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = []
+        cur_length = 0
+        maxl = 0
+        for e in s:
+            if e == ")":
+                if stack:
+                    cur_length += stack.pop() + 2
+                    maxl = max(maxl, cur_length)
+                else:
+                    cur_length = 0
+            elif e == "(":
+                stack.append(cur_length)
+                cur_length = 0
+        return maxl
+        
