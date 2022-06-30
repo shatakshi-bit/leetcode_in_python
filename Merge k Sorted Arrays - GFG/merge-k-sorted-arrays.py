5620fa84eb50e4aaa981aedb5e8d889d02fe44e9
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-06-30 15:39:46 +0530

    Added solution - LeetHub

---
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    #Function to merge k sorted arrays.
+    def mergeKArrays(self, arr, K):
+        s=[]
+        for i in arr:
+            for j in i:
+                s.append(j)
+        return sorted(s)
+        # code here
+        # return merged list
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        numbers=[[ 0 for _ in range(n) ] for _ in range(n) ]
+        line=input().strip().split()
+        for i in range(n):
+            for j in range(n):
+                numbers[i][j]=int(line[i*n+j])
+        ob = Solution();
+        merged_list=ob.mergeKArrays(numbers, n)
+        for i in merged_list:
+            print(i,end=' ')
+        print()
+# } Driver Code Ends
