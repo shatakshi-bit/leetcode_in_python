dc2b4e7943ef73854c0500c49717d5b03be0b556
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-06-30 23:53:27 +0530

    Time: 318 ms (40.85%), Space: 14.2 MB (77.20%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,19 +1,74 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        provinces = 0
-        visited = set()
-    
-        def dfs(node):
-            if node in visited:
-                return
-            visited.add(node)
-            for j, val in enumerate(isConnected[node]):
-                if val == 1 and node != j:
-                    dfs(j)
+        
+        par=[i for i in range(len(isConnected))]
 
-        for i, _ in enumerate(isConnected):
-            if i not in visited:
-                dfs(i)
-                provinces += 1
-        return provinces
+        rank=[1]*(len(isConnected))
+
+        def find(n):
+            p=n
+            while p!=par[p]:
+                par[p]=par[par[p]]
+                p=par[p]
+            return p
+        
+        def union(n1,n2):
+            p1,p2=find(n1),find(n2)
+            if p1==p2:
+                return 0
+
+            if par[p1]>par[p2]:
+                par[p2]=p1
+                rank[p1]+=rank[p2]
+            else:
+
+                par[p1]=p2
+                rank[p2]+=rank[p1]
+            return 1
+
+        count=len(isConnected)
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
+
+                if isConnected[i][j]==1:
+                    count-=union(i,j)
+        return count
+#         n=len(isConnected)
+#         par=[i for i in range(n)]
+#         rank=[1]*n
+        
+#         def find(n1):
+#             res=n1
+#             while res!=par[n1]:
+#                 par[res]=par[par[n1]]
+#                 res=par[n1]
+#             return res
+#         def union(n1,n2):
+#             p1,p2=find(n1),find(n2)
+#             if p1==p2:
+#                 return 0
+#             if rank[p2]>rank[p1]:
+#                 par[p1]=p2
+#                 rank[p2]+=rank[p1]
+#             else:
+#                 par[p2]=p1
+#                 rank[p1]+=rank[p2]
+#             return 1
+#         ans=n
+#         for i in range(n):
+#             for j in range(len(isConnected[i])):
+#                 if isConnected[i][j]==1:
+#                     ans-=union(i,j)
+#         return ans
+                
+                
+                
+            
+        
+    
+            
+            
+            
+            
+        
         
