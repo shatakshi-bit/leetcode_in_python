8cee88f87bb7d669cef7685b4a8ee30051d44366
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-10-06 15:49:03 +0530

    Time: 56 ms (49.32%), Space: 13.9 MB (70.83%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,10 +1,11 @@
 class Solution:
-    def f(self,nums):
-            n=len(nums)
-            prev=nums[0]
+    def rob(self, nums: List[int]) -> int:
+        def f(arr):
+            n=len(arr)
+            prev=arr[0]
             prev2=0
             for i in range(1,n):
-                take=nums[i]
+                take=arr[i]
                 if i>1:
                     take+=prev2
                 nottake=0+prev
@@ -12,9 +13,8 @@ class Solution:
                 prev2=prev
                 prev=curi
             return prev 
-    def rob(self, nums: List[int]) -> int:
         if len(nums)==1:
             return nums[0]
-        return max(self.f(nums[1:len(nums)]),self.f(nums[:-1]))
+        return max(f(nums[1:len(nums)]),f(nums[:-1]))
         
         
