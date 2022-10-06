ac85552ce833f7b680e6b4a2862fb5f210154c47
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-10-06 11:59:55 +0530

    Time: 48 ms (66.52%), Space: 13.9 MB (23.54%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -13,23 +13,8 @@ class Solution:
                 prev=curi
             return prev 
     def rob(self, nums: List[int]) -> int:
-        # def f(arr):
-        #     n=len(arr)
-        #     prev=nums[0]
-        #     prev2=0
-        #     for i in range(1,n):
-        #         take=nums[i]
-        #         if i>1:
-        #             take+=prev2
-        #         nottake=0+prev
-        #         curi=max(take,nottake)
-        #         prev2=prev
-        #         prev=curi
-        #     return prev 
         if len(nums)==1:
             return nums[0]
-        # print(nums[1:len(nums)],nums[:len(nums)-1])
-        # print(f(nums[1:len(nums)]),f(nums[:len(nums)-1]))
         return max(self.f(nums[1:len(nums)]),self.f(nums[:-1]))
         
         
