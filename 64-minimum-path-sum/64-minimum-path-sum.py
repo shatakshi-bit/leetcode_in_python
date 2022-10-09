e4ed51d3de5e69480aaee44ffcf3694485892fe5
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-10-09 20:44:45 +0530

    Time: 222 ms (34.40%), Space: 14.7 MB (97.04%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,16 +1,40 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        for i in range(1, len(grid)):
-            grid[i][0] += grid[i-1][0]
+        #tabulation + space optimization
+        m,n=len(grid),len(grid[0])
+        prev=[0]*n
+        for i in range(m):
+            cur=[0]*n
+            for j in range(n):
+                if i==0 and j==0:
+                    cur[j]=grid[i][j]
+                else:
+                    up=grid[i][j]
+                    if i>0:
+                        up+=prev[j]
+                    else:
+                        up+=1e9
+                    left=grid[i][j]
+                    if j>0:
+                        left+=cur[j-1]
+                    else:
+                        left+=1e9
+                    cur[j]=min(up,left)
+            prev=cur
+        return cur[n-1]
         
-        for j in range(1, len(grid[0])):
-            grid[0][j] += grid[0][j-1]
+#         for i in range(1, len(grid)):
+#             grid[i][0] += grid[i-1][0]
         
-        for i in range(1, len(grid)):
-            for j in range(1, len(grid[0])):
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+#         for j in range(1, len(grid[0])):
+#             grid[0][j] += grid[0][j-1]
+        
+#         for i in range(1, len(grid)):
+#             for j in range(1, len(grid[0])):
+#                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
                 
-        return grid[-1][-1]
+#         return grid[-1][-1]
+    
 #         #recursion
 #         m,n=len(grid),len(grid[0])
 #         def f(i,j):
@@ -40,25 +64,25 @@ class Solution:
 #             return dp[i][j]
 #         return f(m-1,n-1,dp)
     
-        #tabulation
-        m,n=len(grid),len(grid[0])
-        dp=[[0]*n for i in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if i==0 and j==0:
-                    dp[i][j]=grid[i][j]
-                else:
-                    up=grid[i][j]
-                    if i>0:
-                        up+=dp[i-1][j]
-                    else:
-                        up+=1e9
-                    left=grid[i][j]
-                    if j>0:
-                        left+=dp[i][j-1]
-                    else:
-                        left+=1e9
-                    dp[i][j]=min(up,left)    
-        return dp[m-1][n-1]
+#         #tabulation
+#         m,n=len(grid),len(grid[0])
+#         dp=[[0]*n for i in range(m)]
+#         for i in range(m):
+#             for j in range(n):
+#                 if i==0 and j==0:
+#                     dp[i][j]=grid[i][j]
+#                 else:
+#                     up=grid[i][j]
+#                     if i>0:
+#                         up+=dp[i-1][j]
+#                     else:
+#                         up+=1e9
+#                     left=grid[i][j]
+#                     if j>0:
+#                         left+=dp[i][j-1]
+#                     else:
+#                         left+=1e9
+#                     dp[i][j]=min(up,left)    
+#         return dp[m-1][n-1]
                     
                 
