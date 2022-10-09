608852a7640a0af7b0e602a8eb357f8982cbfdda
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2022-10-09 20:38:51 +0530

    Time: 228 ms (30.96%), Space: 15.9 MB (31.22%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,5 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        for i in range(1, len(grid)):
+            grid[i][0] += grid[i-1][0]
+        
+        for j in range(1, len(grid[0])):
+            grid[0][j] += grid[0][j-1]
+        
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+                
+        return grid[-1][-1]
 #         #recursion
 #         m,n=len(grid),len(grid[0])
 #         def f(i,j):
