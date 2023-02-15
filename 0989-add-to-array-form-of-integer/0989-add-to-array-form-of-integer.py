f92acb1af5ea5e7d5c8c9d463e53b24b5cd3fd81
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2023-02-15 15:41:09 +0530

    Time: 278 ms (92.04%), Space: 15.2 MB (36.53%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,13 +1,31 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        strr=""
-        for i in num:
-            strr=strr+str(i)
-        s=str(int(strr)+k)
-        ans=[]
-        for i in s:
-            ans.append(int(i))
-        return ans
+        # strr=""
+        # for i in num:
+        #     strr=strr+str(i)
+        # s=str(int(strr)+k)
+        # ans=[]
+        # for i in s:
+        #     ans.append(int(i))
+        # return ans
+        
+        num.reverse()
+        i=0
+        while k:
+            digit=k%10
+            if i<len(num):
+                num[i]+=digit
+            else:
+                num.append(digit)
+            
+            carry=num[i]//10
+            num[i]=num[i]%10
+            
+            k=k//10
+            k+=carry
+            i=i+1
+            
+        return num[::-1]
         
             
          
