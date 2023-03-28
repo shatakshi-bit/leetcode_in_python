820d393e2fd647397c44463e77c3d305a310129a
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2023-03-28 17:22:06 +0530

    Time: 43 ms (80.13%), Space: 13.9 MB (83.75%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        n = len(days)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        
+        for i in range(1, n + 1):
+            dp[i] = dp[i - 1] + costs[0] # 1-day pass for current day
+            
+            j = i - 1
+            while j >= 0 and days[i - 1] - days[j] < 7:
+                j -= 1
+            dp[i] = min(dp[i], dp[j + 1] + costs[1]) # 7-day pass for current day
+            
+            j = i - 1
+            while j >= 0 and days[i - 1] - days[j] < 30:
+                j -= 1
+            dp[i] = min(dp[i], dp[j + 1] + costs[2]) # 30-day pass for current day
+        
+        return dp[n]
+
+        
