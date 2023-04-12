9248910efea1f7ce6e3d16e684ca3cc05e7bb5f1
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2023-04-12 23:44:40 +0530

    Time: 36 ms (51.06%), Space: 13.8 MB (69.41%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,26 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        #TC and SC-->O(N)
+        stack=[]
+        cur=""
+        for i in path+"/":
+            if i=="/":
+                if cur=="..":
+                    if stack:stack.pop()
+                elif cur!="" and cur!=".":
+                    stack.append(cur)
+                cur=""
+            else:
+                cur+=i
+        return "/"+"/".join(stack)
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
