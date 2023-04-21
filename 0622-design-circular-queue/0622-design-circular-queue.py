2dc7ad9d3eb4f0611581d262a7c3b64fd5a82bc6
shatakshi-bit <60039461+shatakshi-bit@users.noreply.github.com>
2023-04-21 18:21:18 +0530

    Time: 74 ms (39.47%), Space: 14.5 MB (50.73%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,14 +1,14 @@
 class MyCircularQueue:
 
-    def __init__(self, k: int):
-        self.array = [0] * k
+    def __init__(self, p: int):
+        self.array = [0] * p
         self.rear = -1
         self.size = 0
-        self.capacity = k
+        self.c = p
 
     def enQueue(self, value: int) -> bool:
         if self.isFull(): return False
-        self.rear = (self.rear + 1) % self.capacity
+        self.rear = (self.rear + 1) % self.c
         self.array[self.rear] = value
         self.size += 1
         return True
@@ -20,7 +20,7 @@ class MyCircularQueue:
 
     def Front(self) -> int:
         if self.isEmpty(): return -1
-        return self.array[(self.rear-self.size+1) % self.capacity]
+        return self.array[(self.rear-self.size+1) % self.c]
 
     def Rear(self) -> int:
         if self.isEmpty(): return -1
@@ -30,4 +30,4 @@ class MyCircularQueue:
         return not self.size
 
     def isFull(self) -> bool:
-        return self.size == self.capacity
+        return self.size == self.c
